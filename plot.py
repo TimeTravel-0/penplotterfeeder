2560797cca5580bfca1aac1fac9a5b51fb17b6b2
lab <lab@pico>
2015-12-15 23:42:13 +0100

    added multicolor hypnotoad/obey and xkcd pizza, added offset and parameter check

--- plot.py
+++ plot.py
@@ -8,11 +8,23 @@ import sys
 # tested so far:
 # - attached Chaosknoten
 # - Autodesk Spaceshutle Columbia
-#
-# TODO: - make commandline argument handling nicer, wrap code in some function etc.
+
+
+
+if not ( len(sys.argv) == 5 or len(sys.argv) == 6):
+   print "this.py input-file scale offsetx offsety plotter-device"
+   print "this.py drawing.hpgl 3.5 1000 1000 /dev/ttyUSB0"
+   quit()
 
 
 infile = file(sys.argv[1])
+scale = float(sys.argv[2])
+offset = float(sys.argv[3]),float(sys.argv[4])
+
+plotdev = False
+if len(sys.argv) == 6:
+   plotdev = sys.argv[5]
+
 
 intext = infile.read()
 infile.close()
@@ -45,25 +57,41 @@ for item in cmds:
 cmds = outcmds
 
 
-# scaling added, as hpgl files from the web seem to be way too big or way too small
-scale = float(sys.argv[2])
+
+extreme_coordinates = [False, False, False, False]
 
 
-if not sys.argv[3] == "sim":
+
+if plotdev:
    # serial port is opened with rtscts flow control and other setings. Flow control by rtscts allows us to get away
    # without escape sequences to check for a full buffer (e.g. "\x28.B").
-   s = serial.Serial("/dev/ttyUSB0",rtscts=1, baudrate=9600, bytesize=serial.SEVENBITS, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE, timeout=0.01)
+   s = serial.Serial(plotdev,rtscts=1, baudrate=9600, bytesize=serial.SEVENBITS, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE, timeout=0.01)
 
 
 for cmd in cmds:
    if "," in cmd: # oh, something to scale
       x,y = cmd[2:].split(",")
-      cmd = cmd[:2]+str(int(int(x)*scale))+","+str(int(int(y)*scale))
+      x,y = int(int(x)*scale+offset[0]),int(int(y)*scale+offset[1])
+      cmd = cmd[:2]+str(x)+","+str(y)
+
+      if extreme_coordinates[0] == False or x < extreme_coordinates[0]:
+         extreme_coordinates[0] = x
+      if extreme_coordinates[1] == False or y < extreme_coordinates[1]:
+         extreme_coordinates[1] = y
+      if extreme_coordinates[2] == False or x > extreme_coordinates[2]:
+         extreme_coordinates[2] = x
+      if extreme_coordinates[3] == False or y > extreme_coordinates[3]:
+         extreme_coordinates[3] = y
+
+
+
 
    cmd+=";" # every command ends with an ;
    print cmd # display on screen
-   if not sys.argv[3] == "sim":
+   if plotdev:
       s.write(cmd)
 
-if s:
+print "extreme coordinates (x/y in mm) [%f,%f ; %f,%f ] = size (w/h in mm) [%f,%f]"%(extreme_coordinates[0]/40.0, extreme_coordinates[1]/40.0, extreme_coordinates[2]/40.0, extreme_coordinates[3]/40.0, (extreme_coordinates[2]-extreme_coordinates[0])/40.0, (extreme_coordinates[3]-extreme_coordinates[1])/40.0)
+
+if plotdev:
    s.close()
